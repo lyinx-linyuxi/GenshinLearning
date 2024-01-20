29597f483e0089e2c117f3ac5d2ca3cb52c38aa2
lyinx <lyinx123@163.com>
2024-01-20 11:47:27 +0800

    finish lab13

--- CS61a/labs/lab13/lab13.sql
+++ CS61a/labs/lab13/lab13.sql
@@ -2,17 +2,18 @@
 
 
 CREATE TABLE average_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT category, AVG(MSRP) AS average_price FROM products GROUP BY category;
 
 
 CREATE TABLE lowest_prices AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT store, item, min(price) AS lowest_price FROM inventory GROUP BY item;
 
+CREATE TABLE shop AS
+  SELECT name, category FROM products GROUP BY category HAVING min(MSRP/rating);
 
 CREATE TABLE shopping_list AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT a.name, b.store FROM shop AS a, lowest_prices AS b WHERE a.name = b.item;
 
 CREATE TABLE total_bandwidth AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT sum(a.Mbs) FROM stores AS a, shopping_list AS b WHERE a.store = b.store;
 
